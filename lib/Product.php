ea35f4651c04195edeb0efdd9bf0315426bf9ea2
saitoyoshi <mitti180@gmail.com>
2023-06-11 07:15:09 +0900

    型情報を追記

--- lib/Product.php
+++ lib/Product.php
@@ -4,15 +4,15 @@ class Product {
     private string $name;
     private int $price;
 
-    public function __construct($name, $price)
+    public function __construct(string $name, int $price)
     {
         $this->name = $name;
         $this->price = $price;
     }
-    public function getName() {
+    public function getName(): string {
         return $this->name;
     }
-    public function getPrice() {
+    public function getPrice(): int {
         return $this->price;
     }
 }
